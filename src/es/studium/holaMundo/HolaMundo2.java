956ac759a19b6ddd09dc05f850b52c0960f2af99
Teresa-Gabriela <teresagabrielacespedesavalos@gmail.com>
2024-11-24 18:57:11 +0100

    fix: indentación
    Se ha corregido la indentación para facilitar la lectura del código.

--- src/es/studium/holaMundo/HolaMundo2.java
+++ src/es/studium/holaMundo/HolaMundo2.java
@@ -4,12 +4,12 @@ public class HolaMundo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hola mundo ");
-System.out.println("Me encanta programar en Java");
+		System.out.print("Hola mundo ");
+		System.out.println("Me encanta programar en Java");
 
 
 	}
-	}
+}
 
 
 	
