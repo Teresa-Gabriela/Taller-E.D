a2ffe55148260932c661ffdc549a2255eb83d1ac
Teresa-Gabriela <teresagabrielacespedesavalos@gmail.com>
2024-11-24 18:00:33 +0100

    Añadido fichero HolaMundo
    Añadido el fichero con la app "HolaMundo" para
    la asignatura "Entorno de desarrollos".

---
+++ HolaMundo2.java
@@ -0,0 +1,22 @@
+package es.studium.holaMundo;
+
+public class HolaMundo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Hola mundo ");
+System.out.println("Me encanta programar en Java");
+
+
+	}
+	}
+
+
+	
+
+
+	
+
+	
+
+
